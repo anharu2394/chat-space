a7f5bc75838ffe82fff72e21f75f0a34860bf34b
安藤春香 <haruan2394@gmail.com>
2017-12-16 18:40:43 +0900

    行開ける

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
+
   def edit
     @user = User.find(params[:id])
   end
+
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
