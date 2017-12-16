fdb9892988c520b6ebe712ed2199c4052ac9dad2
安藤春香 <haruan2394@gmail.com>
2017-12-16 18:43:05 +0900

    User.find をメゾットにした

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,9 @@
 class UsersController < ApplicationController
-
+ before_action :set_user, only: [:update,:edit]
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to root_path
     else
@@ -18,5 +16,8 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name,:email)
+  end
+  def set_user
+    @user = User.find(params[:id])
   end
 end
