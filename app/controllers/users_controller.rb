b674a455682104d0b754bdcb78c3a3eddd193ba5
安藤春香 <haruan2394@gmail.com>
2017-12-16 18:44:10 +0900

    name email スペース開けた

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name,:email)
+    params.require(:user).permit(:name, :email)
   end
   def set_user
     @user = User.find(params[:id])
