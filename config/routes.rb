078cd048e9977c0ccf5c3cbac1f0a382dbc94ddf
安藤春香 <haruan2394@gmail.com>
2017-12-20 18:40:33 +0900

    only指定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :users,only: [:edit,:update]
-  resources :groups do
-    resources :messages
+  resources :users,only: [:edit, :update]
+  resources :groups,only: [:index, :new, :create, :edit, :update] do
+    resources :messages,only: [:index, :new, :create]
   end
 
   devise_for :users
