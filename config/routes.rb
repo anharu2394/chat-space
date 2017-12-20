646f2c7a8a2af5a7d0f4fd51bf596cd7f7bb0d1c
安藤春香 <haruan2394@gmail.com>
2017-12-20 14:57:58 +0900

    messages routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :users,only: [:edit,:update]
-  resources :groups
+  resources :groups do
+    resources :messages
+  end
+
   devise_for :users
   root 'groups#index'
 
