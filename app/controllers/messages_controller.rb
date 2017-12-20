da157ed36168a9b9dacca8a983ced9fb9224d240
安藤春香 <haruan2394@gmail.com>
2017-12-20 16:34:30 +0900

    send message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,11 +1,24 @@
 class MessagesController < ApplicationController
   def index
     @groups = current_user.groups
+    @message = Message.new
+
   end
 
   def new
   end
 
   def create
+    @message = Message.new(message_params)
+    if @message.save
+      redirect_to root_path, notice: ""
+    else
+      redirect_to group_messages_path, alert: "メッセージを保存できませんでした"
+    end
   end
+
+  private
+    def message_params
+      params.require(:message).permit(:body, :image).merge(user_id: current_user.id, group_id: params[:group_id])
+    end
 end
