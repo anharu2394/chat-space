c8058153b05163b7bc6b5f1af7ba5875a94b8200
安藤春香 <haruan2394@gmail.com>
2018-01-05 16:51:29 +0900

    side barに表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -23,7 +23,7 @@ class MessagesController < ApplicationController
     def message_params
       params.require(:message).permit(:body, :image).merge(user_id: current_user.id, group_id: params[:group_id])
     end
-  def set_group
-    @group = Group.find(params[:group_id])
-  end
+    def set_group
+      @group = Group.find(params[:group_id])
+    end
 end
