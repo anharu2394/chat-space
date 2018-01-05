f3c162d8fdc858d85b162146fdb954f8e0aaf74e
安藤春香 <haruan2394@gmail.com>
2018-01-05 15:42:17 +0900

    グループにメッセージを表示する

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,9 @@
 class MessagesController < ApplicationController
+  before_action :set_group
   def index
     @groups = current_user.groups
     @message = Message.new
+    @messages = @group.messages.includes(:user)
   end
 
   def new
@@ -21,4 +23,7 @@ class MessagesController < ApplicationController
     def message_params
       params.require(:message).permit(:body, :image).merge(user_id: current_user.id, group_id: params[:group_id])
     end
+  def set_group
+    @group = Group.find(params[:group_id])
+  end
 end
